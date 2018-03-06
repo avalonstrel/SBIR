3145e2e1879ab11cc6b0c24f0f3d2cdcad5019d8
avalonstrel <linhy960303@gmail.com>
2018-03-06 22:22:56 +0800

    aa

--- models/triplet_model.py
+++ models/triplet_model.py
@@ -202,7 +202,7 @@ class TripletModel(BaseModel):
             tmp = feat_list[0]
             for feat in feat_list[1:]:
                 tmp = torch.cat([tmp, feat], 0)
-            combined_features[key] = Vairable(tmp)
+            combined_features[key] = Variable(tmp)
         return combined_features
 
     def retrieval_evaluation(self, data, loss,  labels):
