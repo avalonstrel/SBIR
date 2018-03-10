dc048a2ba6a3fc54e00f5224aae7dc0daed67c48
avalonstrel <linhy960303@gmail.com>
2018-03-10 21:32:56 +0800

    aa

--- datasets/imagenet_edgemap_dataset.py
+++ datasets/imagenet_edgemap_dataset.py
@@ -117,6 +117,7 @@ class ImageNetEdgeMapDataset(data.Dataset):
         pil_numpy = np.array(pil)
         print(pil_numpy.shape)
         pil_numpy = self.crop(pil_numpy, bndbox)
+        print(pil_numpy.shape)
         edge_map = cv2.Canny(pil_numpy, 0, 200)
         print(edge_map.shape)
         #edge_map = cv2.resize(edge_map,(self.opt.scale_size,self.opt.scale_size))
