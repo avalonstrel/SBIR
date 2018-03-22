d7f99fce225d0837550fb385ce517b76d9655f0a
avalonstrel <linhy960303@gmail.com>
2018-03-22 13:13:31 +0800

    a

--- options/test_options.py
+++ options/test_options.py
@@ -5,6 +5,7 @@ class TestOptions(BaseOptions):
         BaseOptions.initialize(self)
         self.parser.add_argument('--results_dir', type=str, default='./results/', help='saves results here.')
         self.parser.add_argument('--trained_model', type=str, required=True, help='Load which model to test')
+        self.parser.add_argument('--start_epoch_label', type=str, default='latest', help='Start epoch for continue training')
         self.parser.add_argument('--loss_type', type=str, default='triplet|cls|attr,three_loss', help='The loss for training')        
         self.parser.add_argument('--loss_rate', type=str, default='3.0,0.5,0', help='The loss rate for different loss')
         #self.parser.add_argument('--continue_train', action='store_true', help='continue training: load the start epoch model')
