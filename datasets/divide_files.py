c027169c18c3734ebde31ee419b2a42294ba5d0c
avalonstrel <linhy960303@gmail.com>
2018-03-24 11:14:01 +0800

    a

---
+++ datasets/divide_files.py
@@ -0,0 +1,13 @@
+import os
+root = 'imagenet_train'
+save_root = '/home/lhy/ILSVRC2012_HED'
+for _, _, files in os.walk(root):
+	for file in files:
+		cls_root_i = file.find('_')
+		cls_root = file[:cls_root_i]
+		cls_root = os.path.join(save_root, cls_root)
+		if not os.path.exists(cls_root): 
+			os.system('mkdir '+ cls_root)
+		os.system('cp {} {}'.format(os.path.join(root, file),cls_root))
+		
+
