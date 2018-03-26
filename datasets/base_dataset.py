d9616c942ac1ed7b9ef586b05abacd9ff67d8091
avalonstrel <linhy960303@gmail.com>
2018-03-26 18:39:24 +0800

    A

--- datasets/base_dataset.py
+++ datasets/base_dataset.py
@@ -16,6 +16,9 @@ def create_dataset(opt):
     elif name == 'imagenet':
         from .imagenet_edgemap_dataset import ImageNetEdgeMapDataset
         return ImageNetEdgeMapDataset(opt)
+    elif name == 'imagenet_hed':
+        from .imagenet_hed_dataset import ImageNetHEDDataset
+        return ImageNetEdgeMapDataset(opt)
     elif name == 'tuberlin':
         from .tuberlin_dataset import TUBerlinDataset
         return TUBerlinDataset(opt)
