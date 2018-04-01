c5e5085494b7fa99848ccd10e2e5b36d1ef69812
avalonstrel <linhy960303@gmail.com>
2018-04-02 01:21:02 +0800

    Sphere

--- models/networks.py
+++ models/networks.py
@@ -316,16 +316,21 @@ class AngleLinear(torch.nn.Module):
         phi_theta = phi_theta * xlen.view(-1,1)
         output = (cos_theta,phi_theta)
         return output # size=(B,Classnum,2)
-
+class AngleClassificationNetwork(torch.nn.module):
+    def __init__(self, opt):
+        super(AngleClassificationNetwork, self).__init__()
+        self.angle_linear = AngleLinear(self.opt.feat_size, self.opt.n_fg_labels)
+    def forward(self, x):
+        return self.angle_linear(x)
 class SphereNetwork(torch.nn.Module):
     def __init__(self, opt):
         super(SphereNetwork, self).__init__()
         self.feat_extractor = self.get_extractor(self.opt.feature_model)
-        self.angle_linear = AngleLinear(self.opt.feat_size, self.opt.n_fg_labels)
+        #self.angle_linear = AngleLinear(self.opt.feat_size, self.opt.n_fg_labels)
 
     def forward(self, x):
         out = self.feat_extractor(x)
-        al_out = self.angle_linear(out)
+        #al_out = self.angle_linear(out)
         return al_out
 
     def get_extractor(self, feature_model):
