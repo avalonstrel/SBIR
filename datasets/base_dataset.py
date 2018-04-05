fa1f8f6cf6361a32cc68b132509fcd2cdc3936e1
avalonstrel <linhy960303@gmail.com>
2018-04-05 13:23:34 +0800

    a

--- datasets/base_dataset.py
+++ datasets/base_dataset.py
@@ -16,7 +16,7 @@ def sample_negative(ind, x, search_collection, fg_labels, sample_num, distance_f
     negative_inds = [term[0] for term in sorted_dist[:sample_num[0]]]
     return negative_inds
 
-def hard_negative_mining(model, dataset, query_what, distance_fun, sample_num=(10,100)):
+def hard_negative_mining(model, dataset, query_what, distance_fun, sample_num=(20,100)):
     if query_what == 'image':
         dataset.query_image()
     elif query_what == 'sketch':
