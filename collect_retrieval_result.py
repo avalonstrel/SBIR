84bbb9a86427f01822a0f8162d57fcd19959b96e
avalonstrel <linhy960303@gmail.com>
2018-04-08 19:19:39 +0800

    a

--- collect_retrieval_result.py
+++ collect_retrieval_result.py
@@ -30,74 +30,78 @@ def load_attribute(path):
 
 
 opt = TrainOptions().parse()
-opt = opt
-root = opt.data_root
-flag = opt.loss_flag
-if opt.image_type == 'EDGE':
-    edge_map = True
-else:
-    edge_map = False
-levels = opt.sketch_levels
-attributes = []
-attributes_dict, attribute_size = load_attribute("/home/lhy/datasets/hairstyle_attribute.txt")
-transform_fun = transforms.Compose([transforms.ToTensor()])
-train_split = 20
-mode = opt.phase
-augment_types = opt.augment_types
-#print(flag, edge_map, levels, augment_types)
-# Data Initialization
-photo_imgs = []
-sketch_imgs = []
-photo_neg_imgs = []
-fg_labels = []
-labels = []
+class Dataset():
+    def read_result(opt):
+        self.opt = opt
+        self.root = opt.data_root
+        self.flag = opt.loss_flag
+        if opt.image_type == 'EDGE':
+            self.edge_map = True
+        else:
+            self.edge_map = False
+        self.levels = opt.sketch_levels
+        self.attributes = []
+        self.attributes_dict, self.attribute_size = load_attribute("/home/lhy/datasets/hairstyle_attribute.txt")
+        self.transform_fun = transforms.Compose([transforms.ToTensor()])
+        train_split = 20
+        mode = opt.phase
+        augment_types = opt.augment_types
+        #print(self.flag, self.edge_map, self.levels, augment_types)
+        # Data Initialization
+        self.photo_imgs = []
+        self.sketch_imgs = []
+        self.photo_neg_imgs = []
+        self.fg_labels = []
+        self.labels = []
 
-label = 0
-fg_label = 0
-if mode == "train":
-    start, end = 0, 20
-elif mode == 'test':
-    start, end = 20, 30
-if levels == "stack":
-    levels = "s"
+        label = 0
+        fg_label = 0
+        if mode == "train":
+            start, end = 0, 20
+        elif mode == 'test':
+            start, end = 20, 30
+        if self.levels == "stack":
+            self.levels = "s"
 
-# load pictures
-for root,subFolders,files in os.walk(root):
-    photo_pat = re.compile("cropped_\w+.*\d+.*\.jpg")
-    photo_imgs = list(filter(lambda fname:photo_pat.match(fname),files))
-    if len(photo_imgs) == 0:
-        #print(root)
-        continue
-    sketch_imgs=[]
-    cls_name = root[root.rfind('/')+1:]
-    for i, photo_img in enumerate(photo_imgs):
-        digit = re.findall("\d+",photo_img)[0]
-        if i >= start and i < end :
-            for level in levels:
-                for augment_type in augment_types:
+        # load pictures
+        for root,subFolders,files in os.walk(self.root):
+            photo_pat = re.compile("cropped_\w+.*\d+.*\.jpg")
+            photo_imgs = list(filter(lambda fname:photo_pat.match(fname),files))
+            if len(photo_imgs) == 0:
+                #print(root)
+                continue
+            sketch_imgs=[]
+            cls_name = root[root.rfind('/')+1:]
+            for i, photo_img in enumerate(photo_imgs):
+                digit = re.findall("\d+",photo_img)[0]
+                if i >= start and i < end :
+                    for level in self.levels:
+                        for augment_type in augment_types:
 
-                    flag = "_" if mode == "train" and augment_type != "" else ""
-                    sketch_pat = re.compile("cropped_"+augment_type+flag+str(digit)+level+".*\.png")
-                    sketch_imgs = list(filter(lambda fname:sketch_pat.match(fname),files))
-                    #print(sketch_imgs)
-                    for sketch_img in sketch_imgs:
-                        
-                        photo_imgs.append(os.path.join(root,photo_img))
-                        if levels == "stack":
-                            sketch_other_img = sketch_img.replace("s.","c.")
-                            sketch_ohter_img = sketch_other_img.replace("s_","c_")
-                            sketch_imgs.append([os.path.join(root,sketch_img),os.path.join(root,sketch_other_img)])
-                        else:
+                            flag = "_" if mode == "train" and augment_type != "" else ""
+                            sketch_pat = re.compile("cropped_"+augment_type+flag+str(digit)+level+".*\.png")
+                            sketch_imgs = list(filter(lambda fname:sketch_pat.match(fname),files))
+                            #print(sketch_imgs)
+                            for sketch_img in sketch_imgs:
+                                
+                                self.photo_imgs.append(os.path.join(root,photo_img))
+                                if self.levels == "stack":
+                                    sketch_other_img = sketch_img.replace("s.","c.")
+                                    sketch_ohter_img = sketch_other_img.replace("s_","c_")
+                                    self.sketch_imgs.append([os.path.join(root,sketch_img),os.path.join(root,sketch_other_img)])
+                                else:
 
-                            sketch_imgs.append(os.path.join(root,sketch_img))
-                        photo_neg_imgs.append(os.path.join(root,photo_img))
-                        
-                        attributes.append(attributes_dict[cls_name])
-                        fg_labels.append(fg_label)
-                        labels.append(label)
-            fg_label += 1
-    label += 1
+                                    self.sketch_imgs.append(os.path.join(root,sketch_img))
+                                self.photo_neg_imgs.append(os.path.join(root,photo_img))
+                                
+                                self.attributes.append(self.attributes_dict[cls_name])
+                                self.fg_labels.append(fg_label)
+                                self.labels.append(label)
+                    fg_label += 1
+            label += 1
 
+
+dataset = Dataset().read_result(opt)
 
 retreival_result_file = 'retreival_result_51.txt'
 retrieval_result = read_result(retreival_result_file)
@@ -106,11 +110,11 @@ plt.tight_layout()
 for i, fig, result in enumerate(retrieval_result):
     plt.subplot(5, 11, 11*i)
     #tmp = (sketch_imgs[fig], [photo_imgs[photo_ind] for photo_ind in result])
-    plt.imshow(sketch_imgs[fig])
+    plt.imshow(dataset.sketch_imgs[fig])
     plt.axis('off') 
     for j, photo_ind in enumerate(result):
         plt.subplot(5,11, 11*i+j)
-        plt.imshow(photo_imgs[photo_ind])
+        plt.imshow(dataset.photo_imgs[photo_ind])
         plt.axis('off') 
 plt.savefig('result.png')
 
