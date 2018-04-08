5f8187e998509f22fb00adfefc6bbd66913ddc4f
avalonstrel <linhy960303@gmail.com>
2018-04-08 19:03:09 +0800

    a

--- collect_retrieval_result.py
+++ collect_retrieval_result.py
@@ -9,27 +9,27 @@ from models.base_model import create_model
 import matplotlib.pyplot as plt
 # parameters setting
 opt = TrainOptions().parse()
-self.opt = opt
-self.root = opt.data_root
-self.flag = opt.loss_flag
+opt = opt
+root = opt.data_root
+flag = opt.loss_flag
 if opt.image_type == 'EDGE':
-    self.edge_map = True
+    edge_map = True
 else:
-    self.edge_map = False
-self.levels = opt.sketch_levels
-self.attributes = []
-self.attributes_dict, self.attribute_size = load_attribute("/home/lhy/datasets/hairstyle_attribute.txt")
-self.transform_fun = transforms.Compose([transforms.ToTensor()])
+    edge_map = False
+levels = opt.sketch_levels
+attributes = []
+attributes_dict, attribute_size = load_attribute("/home/lhy/datasets/hairstyle_attribute.txt")
+transform_fun = transforms.Compose([transforms.ToTensor()])
 train_split = 20
 mode = opt.phase
 augment_types = opt.augment_types
-#print(self.flag, self.edge_map, self.levels, augment_types)
+#print(flag, edge_map, levels, augment_types)
 # Data Initialization
-self.photo_imgs = []
-self.sketch_imgs = []
-self.photo_neg_imgs = []
-self.fg_labels = []
-self.labels = []
+photo_imgs = []
+sketch_imgs = []
+photo_neg_imgs = []
+fg_labels = []
+labels = []
 
 label = 0
 fg_label = 0
@@ -37,11 +37,11 @@ if mode == "train":
     start, end = 0, 20
 elif mode == 'test':
     start, end = 20, 30
-if self.levels == "stack":
-    self.levels = "s"
+if levels == "stack":
+    levels = "s"
 
 # load pictures
-for root,subFolders,files in os.walk(self.root):
+for root,subFolders,files in os.walk(root):
     photo_pat = re.compile("cropped_\w+.*\d+.*\.jpg")
     photo_imgs = list(filter(lambda fname:photo_pat.match(fname),files))
     if len(photo_imgs) == 0:
@@ -52,7 +52,7 @@ for root,subFolders,files in os.walk(self.root):
     for i, photo_img in enumerate(photo_imgs):
         digit = re.findall("\d+",photo_img)[0]
         if i >= start and i < end :
-            for level in self.levels:
+            for level in levels:
                 for augment_type in augment_types:
 
                     flag = "_" if mode == "train" and augment_type != "" else ""
@@ -61,19 +61,19 @@ for root,subFolders,files in os.walk(self.root):
                     #print(sketch_imgs)
                     for sketch_img in sketch_imgs:
                         
-                        self.photo_imgs.append(os.path.join(root,photo_img))
-                        if self.levels == "stack":
+                        photo_imgs.append(os.path.join(root,photo_img))
+                        if levels == "stack":
                             sketch_other_img = sketch_img.replace("s.","c.")
                             sketch_ohter_img = sketch_other_img.replace("s_","c_")
-                            self.sketch_imgs.append([os.path.join(root,sketch_img),os.path.join(root,sketch_other_img)])
+                            sketch_imgs.append([os.path.join(root,sketch_img),os.path.join(root,sketch_other_img)])
                         else:
 
-                            self.sketch_imgs.append(os.path.join(root,sketch_img))
-                        self.photo_neg_imgs.append(os.path.join(root,photo_img))
+                            sketch_imgs.append(os.path.join(root,sketch_img))
+                        photo_neg_imgs.append(os.path.join(root,photo_img))
                         
-                        self.attributes.append(self.attributes_dict[cls_name])
-                        self.fg_labels.append(fg_label)
-                        self.labels.append(label)
+                        attributes.append(attributes_dict[cls_name])
+                        fg_labels.append(fg_label)
+                        labels.append(label)
             fg_label += 1
     label += 1
 
@@ -84,12 +84,12 @@ path_result = []
 plt.tight_layout()
 for i, fig, result in enumerate(retrieval_result):
     plt.subplot(5, 11, 11*i)
-    #tmp = (self.sketch_imgs[fig], [self.photo_imgs[photo_ind] for photo_ind in result])
-    plt.imshow(self.sketch_imgs[fig])
+    #tmp = (sketch_imgs[fig], [photo_imgs[photo_ind] for photo_ind in result])
+    plt.imshow(sketch_imgs[fig])
     plt.axis('off') 
     for j, photo_ind in enumerate(result):
         plt.subplot(5,11, 11*i+j)
-        plt.imshow(self.photo_imgs[photo_ind])
+        plt.imshow(photo_imgs[photo_ind])
         plt.axis('off') 
 
 
